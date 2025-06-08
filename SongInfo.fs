4f90e731324370d452280694e0dfc3df40ae2f10
skrim <matias@arje.net>
2025-06-08 09:58:35 +0300

    Add points share to SongInfo

--- SongInfo.fs
+++ SongInfo.fs
@@ -1,18 +1,19 @@
 namespace VoteAnalyzer
 
-type SongInfo =
-    struct
-        val LeagueName: string
-        val RoundName: string
-        val Position: int
-        val PostedBy: string
-        val SongLink: string
-        val SongName: string
-        val ArtistName: string
-        val AlbumName: string
-        val SongId : string
-        val RoundId : string
+    type SongInfo =
+        struct
+            val LeagueName: string
+            val RoundName: string
+            val Position: int
+            val PostedBy: string
+            val SongLink: string
+            val SongName: string
+            val ArtistName: string
+            val AlbumName: string
+            val SongId : string
+            val RoundId : string
+            val PointsShare : float
 
-        new(leagueName: string, roundName: string, songName: string, artistName: string, albumName: string, postedBy: string, songLink: string, position: int, songId : string, roundId: string) =
-            { LeagueName = leagueName; RoundName = roundName; SongName = songName; ArtistName = artistName; AlbumName = albumName; PostedBy = postedBy; SongLink = songLink; Position = position; SongId = songId; RoundId = roundId }
-    end
+            new(leagueName: string, roundName: string, songName: string, artistName: string, albumName: string, postedBy: string, songLink: string, position: int, songId : string, roundId: string, pointsShare: float) =
+                { LeagueName = leagueName; RoundName = roundName; SongName = songName; ArtistName = artistName; AlbumName = albumName; PostedBy = postedBy; SongLink = songLink; Position = position; SongId = songId; RoundId = roundId; PointsShare = pointsShare }
+        end
